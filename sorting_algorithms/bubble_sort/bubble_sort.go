aa9f03ad8fd32527094c06d1cdcb5383cf9275e8
VladanT3 <vladt.t33@gmail.com>
2024-10-25 18:22:30 +0200

    bubble sort, slight improvement

--- sorting_algorithms/bubble_sort/bubble_sort.go
+++ sorting_algorithms/bubble_sort/bubble_sort.go
@@ -5,7 +5,7 @@ package BubbleSort
 
 func bubbleSort(arr []int) []int {
 	var i, j int
-	for i = len(arr) - 1; i >= 0; i-- {
+	for i = len(arr) - 1; i > 0; i-- {
 		for j = 0; j < i; j++ {
 			if arr[j] > arr[j+1] {
 				var temp int = arr[j]
