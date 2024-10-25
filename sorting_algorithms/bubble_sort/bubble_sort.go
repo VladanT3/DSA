e8a1c62eb14b31a24e401612251c21122fd241f6
VladanT3 <vladt.t33@gmail.com>
2024-10-25 18:17:47 +0200

    bubble sort

---
+++ sorting_algorithms/bubble_sort/bubble_sort.go
@@ -0,0 +1,19 @@
+package BubbleSort
+
+//comparing and switching two adjecent elements
+//speed: O(n^2)
+
+func bubbleSort(arr []int) []int {
+	var i, j int
+	for i = len(arr) - 1; i >= 0; i-- {
+		for j = 0; j < i; j++ {
+			if arr[j] > arr[j+1] {
+				var temp int = arr[j]
+				arr[j] = arr[j+1]
+				arr[j+1] = temp
+			}
+		}
+	}
+
+	return arr
+}
