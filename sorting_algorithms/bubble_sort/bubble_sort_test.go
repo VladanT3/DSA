f1c644c1e0361927370fabf884995e2a0396ecc7
VladanT3 <vladt.t33@gmail.com>
2024-10-25 19:07:18 +0200

    it wasnt the last bubble sort update :,(

--- sorting_algorithms/bubble_sort/bubble_sort_test.go
+++ sorting_algorithms/bubble_sort/bubble_sort_test.go
@@ -1,40 +1,30 @@
 package BubbleSort
 
-import "testing"
+import (
+	"testing"
+
+	SortingAlgorithms "github.com/VladanT3/DSA/sorting_algorithms"
+)
 
 func TestBubbleSort(t *testing.T) {
 	res := bubbleSort([]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1})
-	if !equalArrays(res, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}) {
+	if !SortingAlgorithms.EqualArrays(res, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}) {
 		t.Errorf("expected: %d\ngot: %d", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, res)
 	}
 	res = bubbleSort([]int{10, 8, 9, 3, 4, 2, 1, 5, 7, 6})
-	if !equalArrays(res, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}) {
+	if !SortingAlgorithms.EqualArrays(res, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}) {
 		t.Errorf("expected: %d\ngot: %d", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, res)
 	}
 	res = bubbleSort([]int{1, 3, 2, 5, 4, 7, 6, 8, 9})
-	if !equalArrays(res, []int{1, 2, 3, 4, 5, 6, 7, 8, 9}) {
+	if !SortingAlgorithms.EqualArrays(res, []int{1, 2, 3, 4, 5, 6, 7, 8, 9}) {
 		t.Errorf("expected: %d\ngot: %d", []int{1, 2, 3, 4, 5, 6, 7, 8, 9}, res)
 	}
 	res = bubbleSort([]int{1, 3, 6, 5, 6, 7, 9, 8, 9})
-	if !equalArrays(res, []int{1, 3, 5, 6, 6, 7, 8, 9, 9}) {
+	if !SortingAlgorithms.EqualArrays(res, []int{1, 3, 5, 6, 6, 7, 8, 9, 9}) {
 		t.Errorf("expected: %d\ngot: %d", []int{1, 3, 5, 6, 6, 7, 8, 9, 9}, res)
 	}
 	res = bubbleSort([]int{9, 8, 9, 7, 6, 5, 6, 3, 1})
-	if !equalArrays(res, []int{1, 3, 5, 6, 6, 7, 8, 9, 9}) {
+	if !SortingAlgorithms.EqualArrays(res, []int{1, 3, 5, 6, 6, 7, 8, 9, 9}) {
 		t.Errorf("expected: %d\ngot: %d", []int{1, 3, 5, 6, 6, 7, 8, 9, 9}, res)
 	}
-}
-
-func equalArrays(arr1 []int, arr2 []int) bool {
-	if len(arr1) != len(arr2) {
-		return false
-	}
-
-	for i := range arr1 {
-		if arr1[i] != arr2[i] {
-			return false
-		}
-	}
-
-	return true
 }
